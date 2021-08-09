78cb752b4dc9a62c1f6574ff89d9e14ec3029f7a
kazu-taka <k.takahashi@nowall.co.jp>
2021-08-09 22:37:02 +0900

    配列にSwift追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LanguageController extends Controller
 {
     function index() {
-        $langueges = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby', 'Python'];
+        $langueges = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby', 'Python', 'Swift'];
         return view('language.index', ['langueges' => $langueges]);
     }
 }
