f64904e00edd6b3e5c69986a21cd22405a5215b4
kazu-taka <k.takahashi@nowall.co.jp>
2021-08-09 17:38:01 +0900

    配列にPHP追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LanguageController extends Controller
 {
     function index() {
-        $langueges = ['HTML','CSS','JavaScript'];
+        $langueges = ['HTML', 'CSS', 'JavaScript', 'PHP'];
         return view('language.index', ['langueges' => $langueges]);
     }
 }
